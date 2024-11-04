31a4f23b360b034b1c58d8633687b3b4ec970ce6
Tucker <rober3ta@dukes.jmu.edu>
2024-11-04 03:04:27 -0500

    visual indicator for toggling
    Add text box to the toggle object in the xr rig that displays the current movement type.

--- Assets/Scripts/Toggle.cs
+++ Assets/Scripts/Toggle.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
@@ -9,6 +10,8 @@ public class Toggle : MonoBehaviour
 {
     public InputActionReference toggleActionRef;
     public ActionBasedControllerManager rightLocomotion;
+
+    public TextMeshProUGUI text;
     private InputAction toggleAction;
     private Boolean SmoothMotion;
 
@@ -18,7 +21,20 @@ public class Toggle : MonoBehaviour
         toggleAction = toggleActionRef.ToInputAction();
         SmoothMotion = true;
         rightLocomotion.smoothMotionEnabled = false;
+        updateText();
         //Debug.Log("Started");
+    }
+
+    void updateText()
+    {
+        if (!SmoothMotion)
+        {
+            text.SetText("Movement Mode: Free Motion");
+        }
+        else
+        {
+            text.SetText("Movement Mode: Teleportation");
+        }
     }
 
     // Update is called once per frame
@@ -29,6 +45,8 @@ public class Toggle : MonoBehaviour
             //Debug.Log("A button Pressed");
             rightLocomotion.smoothMotionEnabled = SmoothMotion;
             SmoothMotion = !SmoothMotion;
+            //text.SetText("changed");
+            updateText();
         }
     }
 }
