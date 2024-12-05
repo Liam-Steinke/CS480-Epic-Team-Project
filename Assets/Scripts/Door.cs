8c9b36b64d6563e1e766a449aa668f0e817e8c13
Leo Torrijos <torrijld@dukes.jmu.edu>
2024-12-05 17:07:42 -0500

    Doors and Level2 implementation
    Modified the door prefab to allow "opening" and "closing."
    
    Created an implementation of a door: HoloDoor
    
    Added and tested doors to Level2
    
    Level2 geometry edits

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,14 +4,35 @@ using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
+    private BoxCollider collider;
+    public bool startOpen = true;
+    public UnityEvent doorOpened;
+    public UnityEvent doorClosed;
 
-    public UnityEvent door_event;
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (door_event != null)
-        {
-            door_event.Invoke();
+    void Start() {
+        collider = gameObject.GetComponent<BoxCollider>();
+        if (startOpen) {
+            openDoor();
         }
     }
+    
+    public void openDoor() {
+        collider.enabled = false;
+        doorOpened.Invoke();
+    }
+
+    public void closeDoor() {
+        collider.enabled = true;
+        doorClosed.Invoke();
+    }
+
+    
+
+    // void OnTriggerEnter(Collider other)
+    // {
+    //     if (door_event != null)
+    //     {
+    //         door_event.Invoke();
+    //     }
+    // }
 }
